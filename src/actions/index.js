cc19cb25baa7bb7b4f814805b6ead9da1bed8e25
blackmumba <ok.semonenko@gmail.com>
2018-09-04 20:56:22 +0300

    implements actions

---
+++ src/actions/index.js
@@ -0,0 +1,21 @@
+import {ActionTypeEnum} from "../enums/ActionType.enum";
+
+export const addBlock = block => ({
+    type: ActionTypeEnum.ADD_BLOCK,
+    payload: block
+});
+
+export const removeBlock = index => ({
+    type: ActionTypeEnum.REMOVE_BLOCK,
+    payload: index
+});
+
+export const toggleSelectBlock = index => ({
+    type: ActionTypeEnum.TOGGLE_SELECT_BLOCK,
+    payload: index
+});
+
+export const changeColor = index => ({
+    type: ActionTypeEnum.CHANGE_COLOR,
+    payload: index
+});
