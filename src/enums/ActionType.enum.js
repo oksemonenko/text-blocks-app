99e4ab433c9f77c8a2ff39dc90b1819b65ee7833
blackmumba <ok.semonenko@gmail.com>
2018-09-05 17:44:38 +0300

    toggle selection action rename

--- src/enums/ActionType.enum.js
+++ src/enums/ActionType.enum.js
@@ -1,6 +1,6 @@
 export const ActionTypeEnum = {
     ADD_BLOCK: 'Add block',
     REMOVE_BLOCK: 'Remove block',
-    TOGGLE_SELECT_BLOCK: 'Toggle select block',
-    CHANGE_COLOR: 'Change color',
+    TOGGLE_SELECTION: 'Toggle selection',
+    CHANGE_COLOR: 'Change color'
 };
