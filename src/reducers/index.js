405587b47946d803bdfde021824df2b87136ce6a
blackmumba <ok.semonenko@gmail.com>
2018-09-05 17:50:59 +0300

    refactor reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,37 +1,44 @@
 import {ActionTypeEnum} from "../enums/ActionType.enum";
 import {BlockColorEnum} from "../enums/BlockColor.enum";
 
-
 const reducer = (state = [], action) => {
     switch (action.type) {
         case ActionTypeEnum.ADD_BLOCK:
             return [...state, action.payload];
+
         case ActionTypeEnum.REMOVE_BLOCK:
             return [
                 ...state.slice(0, action.payload),
                 ...state.slice(action.payload + 1),
             ];
-        case ActionTypeEnum.TOGGLE_SELECT_BLOCK:
-            const blockToToggleSelect = state[action.payload];
-            blockToToggleSelect.selected = !blockToToggleSelect.selected;
-            return [
-                ...state.slice(0, action.payload),
-                blockToToggleSelect,
-                ...state.slice(action.payload + 1),
-            ];
+
+        case ActionTypeEnum.TOGGLE_SELECTION:
+            return state.map((block, i) => {
+                if (i !== action.payload) {
+                    return block;
+                }
+                return {
+                    ...block,
+                    selected: !block.selected
+                }
+            });
+
         case ActionTypeEnum.CHANGE_COLOR:
-            const blockToChangeColor = state[action.payload];
-            blockToChangeColor.color =
-                blockToChangeColor.color === BlockColorEnum.GREEN ?
-                    BlockColorEnum.RED : BlockColorEnum.GREEN;
-            return [
-                ...state.slice(0, action.payload),
-                blockToChangeColor,
-                ...state.slice(action.payload + 1),
-            ];
+            return state.map((block, i) => {
+                if (i !== action.payload) {
+                    return block;
+                }
+                return {
+                    ...block,
+                    color: block.color === BlockColorEnum.GREEN ?
+                        BlockColorEnum.RED : BlockColorEnum.GREEN
+                }
+            });
+
         default:
             return state;
     }
 };
 
 export default reducer;
+
