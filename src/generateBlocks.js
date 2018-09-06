ec993f161f0576a497ce75c64f9be2ed38fbd57a
blackmumba <ok.semonenko@gmail.com>
2018-09-06 21:22:39 +0300

    change blocks quantity

--- src/generateBlocks.js
+++ src/generateBlocks.js
@@ -2,7 +2,7 @@ import Block from "./components/Block/Block";
 
 export default function generateBlocks () {
     let blocks = [];
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 7; i++) {
         blocks.push(new Block());
     }
     return blocks;
